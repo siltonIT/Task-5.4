511e27eaeb293e8ee9bc4a6fd7d15bcc557ebe6b
siltonIT <140279319+siltonIT@users.noreply.github.com>
2023-12-03 13:59:31 +0300

    fix bags

--- Task 5.4/Task 5.4.cpp
+++ Task 5.4/Task 5.4.cpp
@@ -9,7 +9,7 @@ int main()
 	setlocale(LC_ALL, "");
 
 	const int TEXT_SIZE = 100;
-	char text[TEXT_SIZE] = "привет, как дела Артем, ";
+	char text[TEXT_SIZE] = ", привет, как дела Артем, axax, ";
 	const int SUBTEXT_SIZE = 11;
 	char subtext[SUBTEXT_SIZE] = { 'н', 'а', 'п', 'р', 'и', 'м', 'е', 'р', ',', ' ', '\0'};
 	
@@ -18,7 +18,7 @@ int main()
 		if (text[i] == ',' && text[i + 1] == ' ')
 		{
 			insert(text, TEXT_SIZE, subtext, SUBTEXT_SIZE, i + 1);
-			i += i + 1 + SUBTEXT_SIZE;
+			i += 1 + SUBTEXT_SIZE;
 		}
 	}
 
@@ -33,9 +33,9 @@ void insert(char text[], const int TEXT_SIZE, char subtext[], const int SUBTEXT_
 
 	int counter = 0;
 
-	for (int i = text_len; i < (text_len * 2) - (index + 1); ++i)
+	for (int i = text_len + SUBTEXT_SIZE - 1; i > index + 1; --i)
 	{
-		text[text_len + counter] = text[index + 1 + counter];
+		text[i] = text[text_len - counter];
 		++counter;
 	}
 
@@ -47,15 +47,7 @@ void insert(char text[], const int TEXT_SIZE, char subtext[], const int SUBTEXT_
 		++counter;
 	}
 
-	counter = 0;
-
-	for (int i = index + SUBTEXT_SIZE; i < text_len + SUBTEXT_SIZE - 1; ++i)
-	{
-		text[i] = text[text_len + counter];
-		++counter;
-	}
-
-	text[text_len + SUBTEXT_SIZE - 1] = 0;
+	text[text_len + SUBTEXT_SIZE  - 1] = 0;
 }
 
 int find_len(char text[], const int SIZE)
