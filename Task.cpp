c6a8c79168f32554ad9316a375bbede19fdded17
Sergei <w.silton.it@gmail.com>
2024-05-15 01:01:32 +0300

    Create solution

--- Task.cpp
+++ Task.cpp
@@ -1,5 +1,10 @@
 #include "Task.h"
 
 int main() {
-
+	std::cout << "Write boolean expression: " << std::endl;
+	std::string expression;
+	std::getline(std::cin, expression);
+	
+	Calculator calculator;
+	calculator.solve_expression(expression) ? std::cout << "true" << std::endl : std::cout << "false" << std::endl;
 }
